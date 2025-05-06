4d8e48a48519cce59f7a6c17f1e36f4a041695a4
darzo28 <darzo28@yandex.ru>
2025-05-06 09:31:24 +0300

    +

---
+++ lab4/lab4/lab4.py
@@ -0,0 +1,142 @@
+﻿import sys
+import csv
+from collections import deque
+from collections import defaultdict
+
+EMPTY_IN_SIGNAL = 'ε'
+FINAL_SIGNAL = 'F'
+
+def print_graph(graph, file):
+    with open(file, mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerows(graph)
+
+
+def read_graph(file):
+    f = open(file, 'r', encoding='utf-8')
+    result = []
+    lineCount = 0
+
+    for line in f:
+        if not line.strip():
+            continue
+        splited = line.split(';')
+        result.append([0] * len(splited))
+        for i in range(len(splited)):
+            item = splited[i].strip('\n').strip('\t')
+            if i == 0 or lineCount <= 1:
+                result[lineCount][i] = item
+            else:
+                result[lineCount][i] = item
+
+        lineCount += 1
+
+    return result
+
+
+def get_graph(original):
+    states = []
+    terminals = []
+    transitions = dict()
+
+    for i in range(1, len(original[1])):
+        states.append(original[1][i])
+
+    for i in range(2, len(original)):
+        terminals.append(original[i][0])
+
+    for stateIdx in range(len(states)):
+        transitions[original[1][stateIdx + 1]] = dict()
+        for i in range(2, len(original)):
+            if stateIdx + 1 < len(original[i]) and original[i][stateIdx + 1] != '':
+                if ',' in original[i][stateIdx + 1]:
+                    nextState = original[i][stateIdx + 1].split(',')
+                else:
+                    nextState = [original[i][stateIdx + 1]]
+                transitions[original[1][stateIdx + 1]][original[i][0]] = nextState
+
+    return states, terminals, transitions
+
+
+def eps_closure(state, transitions):
+    eTransitions = set()
+    queue = deque([state])
+
+    while queue:
+        current = queue.popleft()
+        eTransitions.add(current)
+        if EMPTY_IN_SIGNAL in transitions.get(current, {}):
+            for nextState in transitions[current][EMPTY_IN_SIGNAL]:
+                if nextState not in eTransitions:
+                    queue.append(nextState)
+
+    return list(eTransitions)
+
+
+def determine(original, states, terminals, transitions):
+    dfaTerminals = []
+
+    dfaTerminals = [t for t in terminals if t != EMPTY_IN_SIGNAL and t not in dfaTerminals]
+
+    start_closure = eps_closure(states[0], transitions)
+    queue = deque([frozenset(start_closure)])
+    dfaStates = {frozenset(start_closure): f"q0"}
+    dfaTransitions = defaultdict(dict)
+    count = 1
+
+    while queue:
+        current = queue.pop()
+        currentState = dfaStates[frozenset(current)]
+
+        for terminal in dfaTerminals:
+            transitionsSet = set()
+            for prev in current:
+                if terminal in transitions.get(prev, {}):
+                    for nextState in transitions[prev][terminal]:
+                        transitionsSet.update(eps_closure(nextState, transitions))
+
+            if transitionsSet:
+                if frozenset(transitionsSet) not in dfaStates:
+                    newState = f"q{count}"
+                    dfaStates[frozenset(transitionsSet)] = newState
+                    queue.appendleft(transitionsSet)
+                    count += 1
+
+                dfaTransitions[currentState][terminal] = dfaStates[frozenset(transitionsSet)]
+            else:
+                dfaTransitions[currentState][terminal] = ""
+
+    if len(dfaTerminals) != 0:
+        result = [['' for _ in range(len(dfaTransitions) + 1)] for _ in range(len(dfaTerminals) + 2)]
+    else:
+        result = [['' for _ in range(len(dfaTransitions) + 2)] for _ in range(len(dfaTerminals) + 2)]
+
+    for i in range(0, len(dfaTerminals)):
+        result[i + 2][0] = dfaTerminals[i]
+
+    for i, v in enumerate(dfaStates.items()):
+        result[1][i + 1] = v[1]
+        for state in v[0]:
+            if original[0][original[1].index(state)] == 'F':
+                result[0][i + 1] = 'F'
+
+    for k, v in dfaTransitions.items():
+        for next in v.items():
+            result[dfaTerminals.index(next[0]) + 2][result[1].index(k)] = next[1]
+
+    return result
+
+def exit_help():
+    print('lab4.py <input filename> <output filename>')
+    sys.exit(0)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+
+    if len(args) != 2:
+        exit_help()
+
+    original = read_graph(args[0])
+    states, terminals, transitions = get_graph(original)
+    result = determine(original, states, terminals, transitions)
+    print_graph(args[1], result)
---
+++ lab5/lab5/lab5.py
@@ -0,0 +1,211 @@
+﻿import sys
+from lab4 import determine
+
+FINAL_SIGNAL = 'F'
+EMPTY_IN_SIGNAL = 'ε'
+
+class State:
+    def __init__(self, id=None, isFinal=False):
+        self.id = id
+        self.isFinal = isFinal
+        self.transitions = {}
+
+    def addTransition(self, terminal, state):
+        if terminal not in self.transitions:
+            self.transitions[terminal] = []
+        self.transitions[terminal].append(state)
+
+class NFA:
+    def __init__(self, start=None, end=None):
+        self.start = start
+        self.end = end
+
+
+def addConcatenationOperators(regex):
+    result = []
+    prev = None
+    for char in regex:
+        if prev is not None:
+            if (prev.isalnum() or prev == ')' or prev == '*' or prev == '+') and (char.isalnum() or char == '('):
+                result.append('.')
+        result.append(char)
+        prev = char
+    return ''.join(result)
+
+
+def regexToNFA(regex):
+    def applyOperator(operator):
+        nonlocal counter
+        if operator == ".":
+            if len(nfaStack) >= 2:
+                nfa2 = nfaStack.pop()
+                nfa1 = nfaStack.pop()
+                nfa1.end.isFinal = False
+                nfa1.end.addTransition(EMPTY_IN_SIGNAL, nfa2.start)
+                nfaStack.append(NFA(start=nfa1.start, end=nfa2.end))
+
+        elif operator == "|":
+            if len(nfaStack) >= 2:
+                nfa2 = nfaStack.pop()
+                nfa1 = nfaStack.pop()
+
+                start = State()
+
+                end = State(isFinal = True)
+
+                start.addTransition(EMPTY_IN_SIGNAL, nfa1.start)
+                start.addTransition(EMPTY_IN_SIGNAL, nfa2.start)
+
+                nfa1.end.isFinal = False
+                nfa2.end.isFinal = False
+                nfa1.end.addTransition(EMPTY_IN_SIGNAL, end)
+                nfa2.end.addTransition(EMPTY_IN_SIGNAL, end)
+
+                nfaStack.append(NFA(start=start, end=end))
+
+        elif operator == "*":
+            if len(nfaStack) >= 1:
+                nfa = nfaStack.pop()
+                start = State()
+                end = State(isFinal = True)
+                start.addTransition(EMPTY_IN_SIGNAL, nfa.start)
+                start.addTransition(EMPTY_IN_SIGNAL, end)
+
+                nfa.end.isFinal = False
+                nfa.end.addTransition(EMPTY_IN_SIGNAL, nfa.start)
+                nfa.end.addTransition(EMPTY_IN_SIGNAL, end)
+                nfaStack.append(NFA(start=start, end=end))
+
+        elif operator == "+":
+            if len(nfaStack) >= 1:
+                nfa = nfaStack.pop()
+                start = State()
+                end = State(isFinal = True)
+                start.addTransition(EMPTY_IN_SIGNAL, nfa.start)
+
+                nfa.end.isFinal = False
+                nfa.end.addTransition(EMPTY_IN_SIGNAL, nfa.start)
+                nfa.end.addTransition(EMPTY_IN_SIGNAL, end)
+                nfaStack.append(NFA(start=start, end=end))
+
+    def applyBrackets(bracket):
+        if bracket == '(':
+            operatorsStack.append(bracket)
+        elif bracket == ')':
+            while operatorsStack and operatorsStack[-1] != '(':
+                applyOperator(operatorsStack.pop())
+            operatorsStack.pop()
+
+    def makeNFA(ch):
+        nonlocal counter
+        start = State()
+        end = State(isFinal = True)
+        start.addTransition(ch, end)
+        nfaStack.append(NFA(start=start, end=end))
+
+    counter = 0
+    nfaStack = []
+    operatorsStack = []
+    precedence = {"*": 4, "+": 3, ".": 2, "|": 1}
+    regex = addConcatenationOperators(regex).replace('e', EMPTY_IN_SIGNAL)
+
+    print(regex)
+    for ch in regex:
+        if ch.isalnum():
+            makeNFA(ch)
+        elif ch == '(':
+            operatorsStack.append(ch)
+        elif ch == ')':
+            applyBrackets(ch)
+        elif ch in precedence:
+            while (operatorsStack and operatorsStack[-1] in precedence and
+                   precedence[operatorsStack[-1]] >= precedence[ch]):
+                applyOperator(operatorsStack.pop())
+            operatorsStack.append(ch)
+
+
+    while operatorsStack:
+        applyOperator(operatorsStack.pop())
+
+    return nfaStack.pop()
+
+def makeResult(nfa):
+    terminals = set()
+    states = set()
+    finalStates = set()
+    transitions = {}
+
+    counter = 0
+
+    def dfs(state):
+        nonlocal counter
+        if state.id == None:
+            state.id = counter
+            counter += 1
+        states.add(state.id)
+
+        if state.isFinal:
+            finalStates.add(state.id)
+
+        for symbol, next_states in state.transitions.items():
+            for next_state in next_states:
+                if next_state.id == None:
+                    next_state.id = counter
+                    counter += 1
+                if state.id not in transitions:
+                    transitions[state.id] = []
+                transitions[state.id].append((symbol, next_state.id))
+                if symbol not in terminals:
+                    terminals.add(symbol)
+                if next_state.id not in states:
+                    dfs(next_state)
+
+    dfs(nfa.start)
+
+    print(finalStates)
+    terminals = sorted(terminals)
+    states = sorted(states)
+
+    result = [['' for _ in range(len(states) + 1)] for _ in range(len(terminals) + 2)]
+
+    for i, state in enumerate(states):
+        result[1][i + 1] = f"q{state}"
+        if state in finalStates:
+            result[0][i+1] = FINAL_SIGNAL
+
+    for i, terminal in enumerate(terminals):
+        result[i + 2][0] = terminal
+
+    for state, transition_list in transitions.items():
+        state_idx = states.index(state) + 1
+        for symbol, next_state in transition_list:
+            terminal_idx = terminals.index(symbol) + 2
+            if result[terminal_idx][state_idx] == '':
+                result[terminal_idx][state_idx] = f"q{next_state}"
+            else:
+                result[terminal_idx][state_idx] += f",q{next_state}"
+
+    print()
+    for line in result:
+        print(';'.join(line))
+    print()
+
+    return result
+
+def exit_help():
+    print('lab5.py <input> <output>')
+    sys.exit(0)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+
+    if len(args) != 2:
+        exit_help()
+
+    with open(args[0], 'r', encoding='utf-8') as f:
+        regex = f.read().strip()
+
+    nfa = regexToNFA(regex)
+    result = makeResult(nfa)
+    determine(result, args[1])
+    print('Convertion completed')
